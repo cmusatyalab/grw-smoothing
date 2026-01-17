bc74f71852375269f32e82c13372035257ddedac
gilg <gilgoldm@gmail.com>
2026-01-17 06:20:11 -0500

    only prepare test

--- packages/grw-smoothing-models/src/grw_smoothing_models/cli.py
+++ packages/grw-smoothing-models/src/grw_smoothing_models/cli.py
@@ -31,23 +31,23 @@ def kinetics_create_clips(num_workers: int):
     config = GrwSmoothingModelsConfig('../../config.ini').config
     data_home = config.get('kinetics', 'data_home')
 
-    KineticsDs.calculate_metadata(pkl_name='train.pkl',
-                       root=data_home,
-                       split='train',
-                       step_between_clips=5,
-                       num_workers=num_workers,
-                       frames_per_clip=45,
-                       frame_rate=5,
-                       extensions=("avi", "mp4"))
+    # KineticsDs.calculate_metadata(pkl_name='train.pkl',
+    #                    root=data_home,
+    #                    split='train',
+    #                    step_between_clips=5,
+    #                    num_workers=num_workers,
+    #                    frames_per_clip=45,
+    #                    frame_rate=5,
+    #                    extensions=("avi", "mp4"))
 
-    KineticsDs.calculate_metadata(pkl_name='val.pkl',
-                       root=data_home,
-                       split='val',
-                       step_between_clips=5,
-                       num_workers=num_workers,
-                       frames_per_clip=45,
-                       frame_rate=5,
-                       extensions=("avi", "mp4"))
+    # KineticsDs.calculate_metadata(pkl_name='val.pkl',
+    #                    root=data_home,
+    #                    split='val',
+    #                    step_between_clips=5,
+    #                    num_workers=num_workers,
+    #                    frames_per_clip=45,
+    #                    frame_rate=5,
+    #                    extensions=("avi", "mp4"))
 
     KineticsDs.calculate_metadata(pkl_name='test.pkl',
                        root=data_home,
